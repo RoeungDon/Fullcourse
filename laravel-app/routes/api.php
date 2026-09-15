78519593f4228ab4c61b9de627f09e5fae55fe1f
RoeungDon <pudon1898@gmail.com>
2026-09-15 17:16:04 +0700

    session2.2

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\API\AuthController;
 
 Route::apiResource('products', ProductController::class);
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/signin', [AuthController::class, 'signin']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/signout', [AuthController::class, 'signout']);
+    Route::get('/verify', [AuthController::class, 'verify']);
+});
